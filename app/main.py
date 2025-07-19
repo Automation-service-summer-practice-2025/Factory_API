88685f3a8f190348cd4a437f251f28cb9ef6104a
Roman Ovsienko <roma-xom@mail.ru>
2025-07-19 15:53:35 +0600

    feat_add_middleware

--- app/main.py
+++ app/main.py
@@ -2,15 +2,16 @@ from fastapi import FastAPI
 from app.stations.router import router as router_stations
 from app.elements.router import router as router_elements
 from app.documents.router import router as router_documents
+from app.middleware import add_middlewares
 
 
 app = FastAPI()
+add_middlewares(app)
 
 
 @app.get("/")
 async def home():
    return {"data": "Hello World"}
-
 
 app.include_router(router_stations)
 app.include_router(router_elements)
